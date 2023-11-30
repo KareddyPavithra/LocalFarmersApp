f43d21f90d287d17ce3243d46e5ecd6959e3463a
KareddyPavithra <kareddy.p@northeastern.edu>
2023-11-29 19:46:19 -0500

    Changed folder structure

---
+++ src/application/Cart.java
@@ -0,0 +1,36 @@
+//package application;
+//
+//public class Cart {
+//
+//	
+//	private Product product;
+//	private int quantity = 0;
+//	
+//	public Cart(Product product, int quantity) {
+//		this.product = product;
+//		this.quantity = quantity;
+//	}
+//
+//	public Product getProduct() {
+//		return product;
+//	}
+//
+//	public int getQuantity() {
+//		return quantity;
+//	}
+//
+//	public void increaseQuantity() {
+//		this.quantity++;
+//	}
+//	
+//	public void decreaseQuantity() {
+//		if(this.quantity>0) {
+//			this.quantity--;
+//		}
+//	}
+//	
+//	public void resetQuantity() {
+//		this.quantity = 0;
+//	}
+//	
+//}
